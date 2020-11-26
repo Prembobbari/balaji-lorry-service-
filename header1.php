7c635c1f4af8e7b5571827408090415f53875735
Prembobbari <74450938+Prembobbari@users.noreply.github.com>
2020-11-26 21:05:51 +0530

    Add files via upload

---
+++ header1.php
@@ -0,0 +1,25 @@
+<doctypehtml>
+<html>
+    <head>
+      <meta-charset="utf-8">
+        <title>
+          Maths website
+          </title>
+          <metaname="viewport" content="width=device-width,initial-scale=1.0,user-scalable=yes">
+         
+              <link rel="stylesheet" href="styling.css">
+              
+              <body>
+                   <div id="container">
+                
+                         
+                   </div>
+                 
+        
+        
+        
+        
+        </body>
+          </head>
+            </html>
+              </doctypehtml>
